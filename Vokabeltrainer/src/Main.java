4a8e5a5e10df36fa9f52691af3fe6882fad0ac12
ddh950 <117086363+ddh950@users.noreply.github.com>
2024-03-17 23:03:34 +0100

    Correct VocableTrainer

---
+++ Vokabeltrainer/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Gui gui = new Gui();
+		gui.createGui("Vocable");
+		
+//		DatabaseHandler dbh = new DatabaseHandler();
+//		
+//		dbh.removeData();
+
+	}
+
+	
+}
